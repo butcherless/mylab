986ddad3d98bcc1f55336f5697be63955588a49f
butcherless <cmarting@gmail.com>
2013-06-05 22:33:38 +0200

    relationship test interface

--- project-archetype/myarchetype-domain/src/test/java/com/mylab/learn/myarchetype/dao/RelationshipTestInterface.java
+++ project-archetype/myarchetype-domain/src/test/java/com/mylab/learn/myarchetype/dao/RelationshipTestInterface.java
@@ -1,5 +1,34 @@
 package com.mylab.learn.myarchetype.dao;
 
+/**
+ * 
+ * @author cmartin
+ * 
+ */
 public interface RelationshipTestInterface {
 
+	/**
+	 * 
+	 */
+	void testCreateAircraft();
+
+	/**
+	 * 
+	 */
+	void testCreateDestination();
+
+	/**
+	 * 
+	 */
+	void testCreateAircraftWithDestinations();
+
+	/**
+	 * 
+	 */
+	void testAddDestinationToAircraft();
+
+	/**
+	 * 
+	 */
+	void testRemoveDestinationFromAircraft();
 }
