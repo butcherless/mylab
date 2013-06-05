280f3253b5f174229dd0c74fccd3061b89117b1f
butcherless <cmarting@gmail.com>
2013-06-05 02:02:15 +0200

    unused imports

--- project-archetype/myarchetype-domain/src/test/java/com/mylab/learn/myarchetype/dao/TemplateRepositoryUnitTest.java
+++ project-archetype/myarchetype-domain/src/test/java/com/mylab/learn/myarchetype/dao/TemplateRepositoryUnitTest.java
@@ -3,7 +3,6 @@ package com.mylab.learn.myarchetype.dao;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:template-domain-unit-test.xml")
