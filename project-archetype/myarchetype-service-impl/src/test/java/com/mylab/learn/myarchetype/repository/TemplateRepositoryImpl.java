52ee1d11f7018f6753b9a823ce719ace23a1e28e
butcherless <cmarting@gmail.com>
2014-02-27 20:30:49 +0100

    update: maven serveral deps

--- project-archetype/myarchetype-service-impl/src/test/java/com/mylab/learn/myarchetype/repository/TemplateRepositoryImpl.java
+++ project-archetype/myarchetype-service-impl/src/test/java/com/mylab/learn/myarchetype/repository/TemplateRepositoryImpl.java
@@ -11,141 +11,148 @@ import com.mylab.learn.myarchetype.domain.TemplateEntity;
 
 public class TemplateRepositoryImpl implements TemplateRepository {
 
-    @Override
+	@Override
     public void deleteAllInBatch() {
-        // TODO Auto-generated method stub
-        
+	    // TODO Auto-generated method stub
+	    
     }
 
-    @Override
+	@Override
     public void deleteInBatch(Iterable<TemplateEntity> arg0) {
-        // TODO Auto-generated method stub
-        
+	    // TODO Auto-generated method stub
+	    
     }
 
-    @Override
+	@Override
     public List<TemplateEntity> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
 
-    @Override
+	@Override
     public List<TemplateEntity> findAll(Sort arg0) {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
 
-    @Override
+	@Override
     public List<TemplateEntity> findAll(Iterable<Long> arg0) {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
 
-    @Override
+	@Override
     public void flush() {
-        // TODO Auto-generated method stub
-        
+	    // TODO Auto-generated method stub
+	    
     }
 
-    @Override
+	@Override
+    public TemplateEntity getOne(Long arg0) {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	@Override
     public <S extends TemplateEntity> List<S> save(Iterable<S> arg0) {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
 
-    @Override
+	@Override
     public TemplateEntity saveAndFlush(TemplateEntity arg0) {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
 
-    @Override
+	@Override
     public Page<TemplateEntity> findAll(Pageable arg0) {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
 
-    @Override
+	@Override
     public long count() {
-        // TODO Auto-generated method stub
-        return 0;
+	    // TODO Auto-generated method stub
+	    return 0;
     }
 
-    @Override
+	@Override
     public void delete(Long arg0) {
-        // TODO Auto-generated method stub
-        
+	    // TODO Auto-generated method stub
+	    
     }
 
-    @Override
+	@Override
     public void delete(TemplateEntity arg0) {
-        // TODO Auto-generated method stub
-        
+	    // TODO Auto-generated method stub
+	    
     }
 
-    @Override
+	@Override
     public void delete(Iterable<? extends TemplateEntity> arg0) {
-        // TODO Auto-generated method stub
-        
+	    // TODO Auto-generated method stub
+	    
     }
 
-    @Override
+	@Override
     public void deleteAll() {
-        // TODO Auto-generated method stub
-        
+	    // TODO Auto-generated method stub
+	    
     }
 
-    @Override
+	@Override
     public boolean exists(Long arg0) {
-        // TODO Auto-generated method stub
-        return false;
+	    // TODO Auto-generated method stub
+	    return false;
     }
 
-    @Override
+	@Override
     public TemplateEntity findOne(Long arg0) {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
 
-    @Override
+	@Override
     public <S extends TemplateEntity> S save(S arg0) {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
 
-    @Override
+	@Override
     public long count(Specification<TemplateEntity> arg0) {
-        // TODO Auto-generated method stub
-        return 0;
+	    // TODO Auto-generated method stub
+	    return 0;
     }
 
-    @Override
+	@Override
     public List<TemplateEntity> findAll(Specification<TemplateEntity> arg0) {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
 
-    @Override
+	@Override
     public Page<TemplateEntity> findAll(Specification<TemplateEntity> arg0, Pageable arg1) {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
 
-    @Override
+	@Override
     public List<TemplateEntity> findAll(Specification<TemplateEntity> arg0, Sort arg1) {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
 
-    @Override
+	@Override
     public TemplateEntity findOne(Specification<TemplateEntity> arg0) {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
 
-    @Override
+	@Override
     public TemplateEntity findByName(String entityName) {
-        // TODO Auto-generated method stub
-        return null;
+	    // TODO Auto-generated method stub
+	    return null;
     }
+
 }
