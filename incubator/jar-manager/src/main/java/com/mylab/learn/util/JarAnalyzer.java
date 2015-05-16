03edd12462c1ae30e319bf19d5b1c9d414ff6abd
Carlos Martin <cmarting@gmail.com>
2015-05-17 01:19:50 +0200

    find jar files and find class files; java 8 stream processing; tests

--- incubator/jar-manager/src/main/java/com/mylab/learn/util/JarAnalyzer.java
+++ incubator/jar-manager/src/main/java/com/mylab/learn/util/JarAnalyzer.java
@@ -2,12 +2,13 @@ package com.mylab.learn.util;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FileUtils;
@@ -15,69 +16,97 @@ import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JarAnalyzer {
-	
-	final String SHA1SUM_URI = "http://search.maven.org/solrsearch/select?" +
-	"q=1:\"{sha1sum}\"&rows=20&wt=json";
 
-	
-	@Autowired
+    final String SHA1SUM_URI = "http://search.maven.org/solrsearch/select?" +
+            "q=1:\"{sha1sum}\"&rows=20&wt=json";
+
+    @Autowired
     private RestTemplate restTemplate;
-	
-	public Collection<File> findJars(final String pathname) {
-		// file exists
-		// file is a directory
 
-		File file = new File(pathname);
-		String[] extensions = { "jar" };
-		Boolean recursive = Boolean.TRUE;
+    public Collection<JarFileBean> findJarFiles(final String pathname) {
+        // validation:
+        // -file exists
+        // -file is a directory
+        String[] extensions = { "jar" };
+        File file = new File(pathname);
+        Collection<File> files = FileUtils.listFiles(file, extensions, Boolean.TRUE);
+        files.stream().map(f -> this.createJarFileBean(f));
 
-		return FileUtils.listFiles(file, extensions, recursive);
-	}
+        return files.stream()
+                .map(f -> this.createJarFileBean(f))
+                .collect(Collectors.toList());
+    }
 
-	public JarFileBean createJarFileBean(final File file)
-			throws FileNotFoundException, IOException {
-		String path = FilenameUtils.getPath(file.getPath());
-		String name = FilenameUtils.removeExtension(FilenameUtils.getName(file
-				.getPath()));
-		String sha1Hex = DigestUtils.sha1Hex(new FileInputStream(file));
+    public JarFileBean createJarFileBean(final File file) {
+        String path = FilenameUtils.getPath(file.getPath());
+        String name = FilenameUtils.removeExtension(FilenameUtils.getName(file.getPath()));
+        String sha1Hex;
+        try {
+            sha1Hex = DigestUtils.sha1Hex(new FileInputStream(file));
+        } catch (IOException e) {
+            throw new JarAnalizerException("unable to calculate sha1sum", e);
+        }
 
-		return new JarFileBean(name, path, sha1Hex);
-	}
+        return new JarFileBean(name, path, sha1Hex);
+    }
 
-	public ClassFileBean createClassFileBean(final JarEntry entry) {
+    public ClassFileBean createClassFileBean(final JarEntry entry) {
 
-		String fullname = FilenameUtils.removeExtension(entry.getName()).replace('/', '.');
-		Long size = entry.getSize();
+        String fullname = FilenameUtils.removeExtension(entry.getName()).replace('/', '.');
+        Long size = entry.getSize();
+        if (size < 0) {
+            throw new JarAnalizerException("Class size cannot be negative: " + fullname);
+        }
 
-		return new ClassFileBean(fullname, size);
-	}
-	
-	public String findJarFileBySha1sum(final String sha1sum) throws IOException {
-		RestTemplate restTemplate = new RestTemplate();
+        return new ClassFileBean(fullname, size);
+    }
 
-		Map<String, String> params = new HashMap<String, String>();
-		params.put("sha1sum", sha1sum);
+    public String findJarFileBySha1sum(final String sha1sum) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
 
-		String result = restTemplate.getForObject(SHA1SUM_URI, String.class, params);
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("sha1sum", sha1sum);
 
-		this.parseResponse(result);		
-		
-		return result;
-	}
-	
-	private void parseResponse(String response) throws IOException {
-		//create ObjectMapper instance
-		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode rootNode = objectMapper.readTree(response);
-		JsonNode responseNode = rootNode.path("response");
-		System.out.println("missing: " + responseNode.isMissingNode());
-		System.out.println("numFound: " + responseNode.path("numFound"));
-	}
+        String result = restTemplate.getForObject(SHA1SUM_URI, String.class, params);
+
+        this.parseResponse(result);
+
+        return result;
+    }
+
+    private void parseResponse(String response) throws IOException {
+        // create ObjectMapper instance
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(response);
+        JsonNode responseNode = rootNode.path("response");
+        System.out.println("missing: " + responseNode.isMissingNode());
+        System.out.println("numFound: " + responseNode.path("numFound"));
+    }
+
+    public Collection<ClassFileBean> findClassFileByJarFile(final String jarFilePath) {
+        System.out.println("processing jarfile: " + jarFilePath);
+
+        JarFile jarFile = null;
+
+        try {
+            jarFile = new JarFile(jarFilePath);
+
+            return jarFile.stream()
+                    .filter(e -> e.getName().matches(".*class$"))
+                    .map(e -> this.createClassFileBean(e))
+                    .collect(Collectors.toSet());
+
+        } catch (IOException e) {
+            throw new JarAnalizerException("Unable to read jar file: " + jarFilePath, e);
+        } finally {
+            try {
+                jarFile.close();
+            } catch (IOException e) {
+            }
+        }
+    }
 }
--- incubator/jar-manager/src/test/java/com/mylab/learn/util/JarAnalyzerTest.java
+++ incubator/jar-manager/src/test/java/com/mylab/learn/util/JarAnalyzerTest.java
@@ -1,11 +1,9 @@
 package com.mylab.learn.util;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
 
@@ -14,62 +12,71 @@ import org.junit.Test;
 
 public class JarAnalyzerTest {
 
-	final String PATH = "/home/cmartin/.m2/repository";
-	JarAnalyzer analyzer = new JarAnalyzer();
-	String jarFilePath = "/home/cmartin/.m2/repository/org/springframework/spring-core/4.1.6.RELEASE/spring-core-4.1.6.RELEASE.jar";
-	
-	@Test
-	public void testCreateJarFiles() throws FileNotFoundException, IOException {
-		Collection<File> jarList = this.analyzer.findJars(PATH);
-		Assert.assertFalse(jarList.isEmpty());
-		this.printJarCollection(jarList);
-	}
+    final String PATH = "/home/cmartin/.m2/repository";
+    JarAnalyzer analyzer = new JarAnalyzer();
+    String jarFilePath = "/home/cmartin/.m2/repository/org/springframework/spring-core/4.1.6.RELEASE/spring-core-4.1.6.RELEASE.jar";
 
-	@Test
-	public void testCreateJarEntries() throws FileNotFoundException, IOException {
-		Collection<File> jarList = this.analyzer.findJars(PATH);
-		
-		for (Iterator<File> iterator = jarList.iterator(); iterator.hasNext();) {
-			File file = iterator.next();
-			JarInputStream jis = new JarInputStream(new FileInputStream(file.getCanonicalPath()));
-			JarEntry entry;
-			while ((entry = jis.getNextJarEntry()) != null) {
-				String name = entry.getName();
-				if (name.matches(".*class$")) {
-					ClassFileBean classFileBean = this.analyzer.createClassFileBean(entry);
-					System.out.println(classFileBean);
-				}
-			}
-		}
-	}
+    @Test
+    public void testFindJarFiles() throws FileNotFoundException, IOException {
+        Collection<JarFileBean> jarFileList = this.analyzer.findJarFiles(PATH);
+        Assert.assertFalse(jarFileList.isEmpty());
+        jarFileList.forEach(f -> System.out.println(f));
+    }
 
-	@Test
-	public void testCreateJarEntriesFromJar() throws FileNotFoundException, IOException {
-		JarInputStream jis = new JarInputStream(new FileInputStream(jarFilePath));
-		JarEntry entry;
-		while ((entry = jis.getNextJarEntry()) != null) {
-			String name = entry.getName();
-			if (name.matches(".*class$")) {
-				ClassFileBean classFileBean = this.analyzer.createClassFileBean(entry);
-				System.out.println(classFileBean);
-			}
-		}
-	}
-	
-	@Test
-	public void testfindJarFileBySha1sum() throws IOException {
-		String sha1sum = "19d4e90b43059058f6e056f794f0ea4030d60b86";
-		String result = this.analyzer.findJarFileBySha1sum(sha1sum);
-		System.out.println(result);
-	}
-	
-	
-	private void printJarCollection(final Collection<File> collection)
-			throws FileNotFoundException, IOException {
-		System.out.println("collection count: " + collection.size());
-		for (Iterator<File> iterator = collection.iterator(); iterator.hasNext();) {
-			File file = (File) iterator.next();
-			System.out.println(this.analyzer.createJarFileBean(file));
-		}
-	}
+    @Test
+    public void testFindClassesByJarFile() {
+        Collection<ClassFileBean> classList = this.analyzer
+                .findClassFileByJarFile(this.jarFilePath);
+        Assert.assertFalse("jar must have java classes", classList.isEmpty());
+        classList.forEach(c -> System.out.println(c));
+    }
+
+    @Test
+    public void test() throws IOException {
+        Collection<JarFileBean> jarFileList = this.analyzer.findJarFiles(PATH);
+        jarFileList.stream()
+                .forEach(f -> this.analyzer.findClassFileByJarFile(f.getFullname()));
+    }
+
+    @Test
+    public void testfindJarFileBySha1sum() throws IOException {
+        String sha1sum = "19d4e90b43059058f6e056f794f0ea4030d60b86";
+        String result = this.analyzer.findJarFileBySha1sum(sha1sum);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testCreateJarEntries() throws FileNotFoundException, IOException {
+        // Collection<File> jarList = this.analyzer.findJarFiles(PATH);
+        //
+        // for (Iterator<File> iterator = jarList.iterator();
+        // iterator.hasNext();) {
+        // File file = iterator.next();
+        // JarInputStream jis = new JarInputStream(new
+        // FileInputStream(file.getCanonicalPath()));
+        // JarEntry entry;
+        // while ((entry = jis.getNextJarEntry()) != null) {
+        // String name = entry.getName();
+        // if (name.matches(".*class$")) {
+        // ClassFileBean classFileBean =
+        // this.analyzer.createClassFileBean(entry);
+        // System.out.println(classFileBean);
+        // }
+        // }
+        // }
+    }
+
+    @Test
+    public void testCreateJarEntriesFromJar() throws FileNotFoundException, IOException {
+        JarInputStream jis = new JarInputStream(new FileInputStream(jarFilePath));
+        JarEntry entry;
+        while ((entry = jis.getNextJarEntry()) != null) {
+            String name = entry.getName();
+            if (name.matches(".*class$")) {
+                ClassFileBean classFileBean = this.analyzer.createClassFileBean(entry);
+                System.out.println(classFileBean);
+            }
+        }
+    }
+
 }
