b4fe695dea2e90002c56b6d05f7926aef2214a89
butcherless <cmarting@gmail.com>
2015-05-19 09:05:48 +0200

    jar-analyzer project creation

---
+++ incubator/jar-analyzer/jar-analyzer-domain/src/main/java/com/mylab/learn/jaranalyzer/domain/JarFileBean.java
@@ -0,0 +1,37 @@
+package com.mylab.learn.jaranalyzer.domain;
+
+import org.springframework.data.neo4j.annotation.GraphId;
+import org.springframework.data.neo4j.annotation.NodeEntity;
+
+@NodeEntity
+public class JarFileBean {
+	@GraphId
+	Long nodeId;
+	
+	private final String name;
+	private final String path;
+	private final String sha1Hex;
+
+	public JarFileBean(String name, String path, String sha1Hex) {
+		this.name = name;
+		this.path = path;
+		this.sha1Hex = sha1Hex;
+	}
+
+	public String getFullname() {
+		return new StringBuilder("/").append(path).append(name).append(".jar")
+				.toString();
+	}
+
+	public String getSha1Hex() {
+		return this.sha1Hex;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return new StringBuffer().append("JarFileBean: name=")
+				.append(this.name).append(",path=").append(this.path)
+				.append(",sha1Hex=").append(this.sha1Hex).toString();
+	}
+}
