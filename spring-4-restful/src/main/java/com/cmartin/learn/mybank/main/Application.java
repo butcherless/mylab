83f5fecf77077668d655f6728f0e1f53f4a36676
Cristian Romero Matesanz <cristianprofile@gmail.com>
2016-07-21 14:53:56 +0200

    Updated Port
    Updated Spring boot app port and run boot app in main method runner.

--- spring-4-restful/src/main/java/com/cmartin/learn/mybank/main/Application.java
+++ spring-4-restful/src/main/java/com/cmartin/learn/mybank/main/Application.java
@@ -21,5 +21,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
+        //do not show Spring boot banner when boot starts!!!!!
+        app.run(args);
     }
 }
