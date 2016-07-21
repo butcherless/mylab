5192793941bd07cb47c6b11c59691bd7fac74608
Cristian Romero Matesanz <cristianprofile@gmail.com>
2016-07-21 15:08:35 +0200

    Deleted code and configuration class

--- spring-4-restful/src/main/java/com/cmartin/learn/mybank/main/Application.java
+++ spring-4-restful/src/main/java/com/cmartin/learn/mybank/main/Application.java
@@ -21,7 +21,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
-        //do not show Spring boot banner when boot starts!!!!!
         app.run(args);
     }
 }
