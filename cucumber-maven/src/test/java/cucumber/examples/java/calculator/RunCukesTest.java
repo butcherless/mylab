1658fc102188367d279e88554927f984b5da673f
Carlos Martin <cmarting@gmail.com>
2018-01-21 23:26:44 +0100

    add cucumber 2.3 project

---
+++ cucumber-maven/src/test/java/cucumber/examples/java/calculator/RunCukesTest.java
@@ -0,0 +1,13 @@
+package cucumber.examples.java.calculator;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-report"}
+        //tags = {"@arithmetic", "@date", "not @shopping"}
+)
+public class RunCukesTest {
+}
